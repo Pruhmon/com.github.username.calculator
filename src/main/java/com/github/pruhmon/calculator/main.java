9739e50e23b98d74616f7884ad02fc2208cfaf5d
Praman White <Pruhmon@gmail.com>
2025-03-18 13:13:34 -0700

    trying

---
+++ src/main/java/com/github/pruhmon/calculator/main.java
@@ -0,0 +1,13 @@
+package com.github.pruhmon.calculator;
+
+public class main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        int sum = calculator.add(5, 3);
+        System.out.println("5 + 3 = " + sum);
+
+    }
+}
+
+}
