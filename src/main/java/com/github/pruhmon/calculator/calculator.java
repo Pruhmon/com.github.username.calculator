8cadc25ac376a005b7fd0af42c350840321b442f
Praman <Pruhmon@gmail.com>
2025-03-19 13:30:34 -0700

    Update calculator.java

--- src/main/java/com/github/pruhmon/calculator/calculator.java
+++ src/main/java/com/github/pruhmon/calculator/calculator.java
@@ -9,10 +9,40 @@ public class Calculator {
         return a - b;
     }
 
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
     public int divide(int a, int b) {
         if (b == 0) {
             throw new ArithmeticException("Cannot divide by zero");
         }
         return a / b;
+    }
+
+    public int modulo(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot modulo by zero");
+        }
+        return a % b;
+    }
+
+    public double power(double base, double exponent) {
+        return Math.pow(base, exponent);
+    }
+
+    public int absolute(int a) {
+        return Math.abs(a);
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println("Addition: " + calc.add(10, 5));
+        System.out.println("Subtraction: " + calc.subtract(10, 5));
+        System.out.println("Multiplication: " + calc.multiply(10, 5));
+        System.out.println("Division: " + calc.divide(10, 5));
+        System.out.println("Modulo: " + calc.modulo(10, 3));
+        System.out.println("Power: " + calc.power(2, 3));
+        System.out.println("Absolute: " + calc.absolute(-10));
     }
 }
